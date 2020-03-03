e9f9843462c0713b16d0bb234455538da7cfca62
noel_yulon_energe <noel.sung@yulon-energy.com.tw>
2020-03-03 14:37:55 +0800

    init

---
+++ app/src/main/java/tw/noel/sung/com/library_ui_linkview/link_view/implement/OnDrawLineFinishedListener.java
@@ -0,0 +1,9 @@
+package tw.noel.sung.com.library_ui_linkview.link_view.implement;
+
+/**
+ * Created by noel on 2018/5/1.
+ */
+
+public interface OnDrawLineFinishedListener {
+    void onDrawLineFinished(String password);
+}
